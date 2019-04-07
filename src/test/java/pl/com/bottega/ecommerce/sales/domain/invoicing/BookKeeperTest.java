42d313dac8f4bfc52c06f768b57693973edb8faa
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-04-07 14:53:41 +0200

    isolation testRequestInvoiceOnePositionShouldReturnOne

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,39 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.client.Client;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import static org.mockito.Mockito.*;
+
+public class BookKeeperTest {
+
+    @Test public void testRequestInvoiceOnePositionShouldReturnOne() {
+        Id id = new Id("1");
+        ClientData client = new ClientData(id, "Piotrek");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(client);
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+
+        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        when(taxPolicy.calculateTax(ProductType.FOOD, new Money(10))).thenReturn(new Tax(new Money(10), "5%"));
+
+        ProductData productData = mock(ProductData.class);
+        when(productData.getType()).thenReturn(ProductType.FOOD);
+
+        RequestItem requestItem = new RequestItem(productData, 5, new Money(10));
+        invoiceRequest.add(requestItem);
+
+        Invoice invoiceResult = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        assertThat(invoiceResult.getItems().size(), is(1));
+    }
+}
